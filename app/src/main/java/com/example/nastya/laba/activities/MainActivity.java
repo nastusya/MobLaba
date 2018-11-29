138eae4d913613b153dd9da2d12fbf675df92a22
nastusya <nastasuper5@gmail.com>
2018-11-29 20:22:07 +0200

    resolve changes2

--- app/src/main/java/com/example/nastya/laba/activities/MainActivity.java
+++ app/src/main/java/com/example/nastya/laba/activities/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        //bottomNavigation.setOnNavigationItemSelectedListener(navigationListener);
         setFragment(new ListFragment(), false);
     }
 
